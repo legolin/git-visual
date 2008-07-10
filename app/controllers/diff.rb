5b0eee96a71944e74c875856070b98d168a558bf
Daniel Parker <dcparker@gmail.com>
2008-07-09 21:37:52 -0400

    Viewing diff works again with merb-0.9.3

--- app/controllers/diff.rb
+++ app/controllers/diff.rb
@@ -2,6 +2,6 @@ class Diff < Application
   def show(from, to, path=nil)
     @diff = Git::Diff.new(mygit, from, to, path)
     diff = @diff.patch
-    render :text => (from == 'index' && to == 'working' && File.exists?(mygit.dir.path + '/' + path) && diff.length == 0) ? `cat "#{mygit.dir.path}/#{path}"` : diff
+    render ((from == 'index' && to == 'working' && File.exists?(mygit.dir.path + '/' + path) && diff.length == 0) ? `cat "#{mygit.dir.path}/#{path}"` : diff), :format => :text
   end
 end
